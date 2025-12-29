f3f9907f98e405a779184c0af5e0b625b86a1b0e
thippeshdevops26 <thippeshdevops26@gmail.com>
2025-12-29 14:58:20 +0530

    Initial API automation framework

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,16 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features/postman_echo.feature",
+        glue = "stepdefinitions",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html"
+        },
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
